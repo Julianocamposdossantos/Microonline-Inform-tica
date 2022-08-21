a8095d5754c291424c7c631020fe28a1ae5f65b4
Juliano Campos <88381643+Julianocamposdossantos@users.noreply.github.com>
2022-08-21 15:37:03 -0300

    Add files via upload

---
+++ js/Microonlline.js
@@ -0,0 +1,12 @@
+ScrollReveal({
+	origin: 'left',
+	distance:'90px',		
+	duraction: 700
+}).reveal('.colunas');
+
+
+// ScrollReveal({
+// 	origin:'left',
+// 	distance:'30px',
+// 	duration: 700,
+// }).reveal('#home, #home img, #home .stats, #services, #services header, #services .card	');
